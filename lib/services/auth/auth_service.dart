af4b040b8bebb6c3f0bb64c6c0b0d2efb1f806df
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-05-31 10:12:54 -0300

    aula 25

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,10 +1,13 @@
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
 
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
   AuthService(this.provider);
+
 
   @override
   Future<AuthUser> createUser({
@@ -28,4 +31,9 @@ class AuthService implements AuthProvider {
 
   @override
   Future<void> sendEmailVerification() => provider.sendEmailVerification();
+
+  @override
+  Future<void> initialize() async {
+    provider.initialize();
+  }
 }
